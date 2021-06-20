3b3684d9ceb7471f31a235edbf490d8b547eb59a
prasen-jha <parsen.jha@gmail.com>
2021-06-20 10:47:44 +0530

    code comment in parse_product

--- amznSpyder/amznSpyder/spiders/spyder.py
+++ amznSpyder/amznSpyder/spiders/spyder.py
@@ -1,7 +1,4 @@
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-
 
 class AmznSpider(scrapy.Spider):
     name = 'amazon'
@@ -9,23 +6,14 @@ class AmznSpider(scrapy.Spider):
     allowed_domains = ['www.amazon.in']
     start_urls = ['https://www.amazon.in/s?k=ginger+powder']
 
-    product_link = LinkExtractor(restrict_xpaths='//div//h2/a/@href')
-    rule_produt_link = Rule(product_link, callback='parse', follow=False)
-
-    rules = (
-        rule_produt_link,
-    )
-
     def parse(self, response, **kwargs):
         for link in response.xpath("//div//h2/a/@href").extract():
             url = 'https://www.amazon.in' + link
-            yield scrapy.Request(url, callback=self.parse_product,
-                                 headers={
-                                     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:90.0) Gecko/20100101 Firefox/90.0'})
+            yield scrapy.Request(url, callback=self.parse_product,)
 
     def parse_product(self, response):
         yield {
             'product_name': str(response.xpath('//span[@id="productTitle"]/text()').get()).strip(),
-            'rating': response.xpath(""),
+            # 'rating': response.xpath(""),
             'link': response.url,
         }
